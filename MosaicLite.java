619e0d9a8b210cce84bba4f94be9c32c3cb59a04
Mohammad Alabed <59902565+boomsquad@users.noreply.github.com>
2020-02-27 17:17:10 -0600

    Implement JFrame

---
+++ MosaicLite.java
@@ -0,0 +1,25 @@
+import javax.swing.JFrame;
+
+
+
+
+
+class MosaicLiteFrame extends JFrame 
+{
+    public MosaicLiteFrame()
+    {
+        setBounds(200,200,1200,800);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
+
+public class MosaicLite
+{
+    public static void main(String[] args)
+    {
+        System.out.println("MosaicLite Starting...");
+
+        MosaicLiteFrame myFrame = new MosaicLiteFrame();
+        myFrame.setVisible(true);
+    }
+}
